809c8c9611b8604a5122977964f43110cb58768f
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2024-11-11 10:35:10 -0600

    Update App.java

--- hola-mundo/src/main/java/com/ejemplo/App.java
+++ hola-mundo/src/main/java/com/ejemplo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola mundo desde participante JBCR, despues del pull request! v1.0" );
+        System.out.println( "Hola mundo desde participante JBCR, despues del pull request!" );
     }
 }
